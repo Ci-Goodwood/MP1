23e5779933a7e13740d96276702e07e55e3a2a2f
Ci-Goodwood <3111895209@qq.com>
2025-05-13 18:31:51 +0800

    update name and headline

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,11 +3,11 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = '嘉州镜'
+export const headline = '一个前端工作者'
 export const introduction =
   "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
-export const email = 'tong.hu@scls-sh.org'
+export const email = '3111895209@qq.com'
 export const githubUsername = 'derekhut'
 
 // about page
