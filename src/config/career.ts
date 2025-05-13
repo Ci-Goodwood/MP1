56ab687f8058810f34936893eec6a0f7d5d244a6
Ci-Goodwood <3111895209@qq.com>
2025-05-13 20:12:03 +0800

    career

--- src/config/career.ts
+++ src/config/career.ts
@@ -16,25 +16,5 @@ export const careerList: Array<CareerItemType> = [
       start: '2020',
       end: 'Present'
     },
-    {
-      company: 'Bigo Live',
-      title: 'US Operations',
-      logo: 'coffee',
-      start: '2018',
-      end: '2020'
-    },
-    {
-      company: 'Sinovation Ventures',
-      title: 'Investment Analyst',
-      logo: 'bank',
-      start: '2017',
-      end: '2018'
-    },
-    {
-      company: 'Expedia',
-      title: 'Software Engineer',
-      logo: 'coffee',
-      start: '2015',
-      end: '2017'
-    }
+    
   ]
